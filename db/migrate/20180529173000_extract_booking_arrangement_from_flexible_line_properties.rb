d080e2ed30c6d5a0c2a55e45af5982d677cf9ae6
vincent <vincent.paturet@entur.org>
2022-03-04 13:20:01 +0100

    Fix migration typo

--- db/migrate/20180529173000_extract_booking_arrangement_from_flexible_line_properties.rb
+++ db/migrate/20180529173000_extract_booking_arrangement_from_flexible_line_properties.rb
@@ -28,7 +28,7 @@ class ExtractBookingArrangementFromFlexibleLineProperties < ActiveRecord::Migrat
     add_foreign_key "booking_arrangements", "contact_structures", column: "booking_contact_id", name: "booking_arrangement_booking_contact_fkey"
 
 
-    remove_foreign_key :lines, :booking_contacts
+    remove_foreign_key :lines, :contact_structures
     remove_column :lines, :booking_note, :string, null: true
     remove_column :lines, :booking_access, :string, null: true
     remove_column :lines, :book_when, :string, null: true
