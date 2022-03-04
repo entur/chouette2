82a4edd3a75d208d1d15481decc0dfe98779c8de
vincent <vincent.paturet@entur.org>
2022-03-04 11:54:19 +0100

    Update migrations for compatibility with Rails 5

--- db/migrate/20180529173000_extract_booking_arrangement_from_flexible_line_properties.rb
+++ db/migrate/20180529173000_extract_booking_arrangement_from_flexible_line_properties.rb
@@ -1,4 +1,4 @@
-class ExtractBookingArrangementFromFlexibleLineProperties < ActiveRecord::Migration
+class ExtractBookingArrangementFromFlexibleLineProperties < ActiveRecord::Migration[4.2]
   def change
 
     create_table :booking_arrangements, :force => true do |t|
