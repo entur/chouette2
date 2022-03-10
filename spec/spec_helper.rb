1953129baa18b8d043eeceaf3e79250619ef144a
vincent <vincent.paturet@entur.org>
2022-03-10 10:07:13 +0100

    Fix deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -82,4 +82,6 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.allow_message_expectations_on_nil = true
 end
